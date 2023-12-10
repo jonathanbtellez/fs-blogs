a908dc0247294acf60d088aceef8acc198aa5416
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-10 14:41:20 -0500

    4.1

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-require('dotenv').config()
+const app = require('./app') // la aplicación Express real
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const {MONGODB_URI} = process.env
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/v1/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/v1/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const { PORT } = process.env
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
