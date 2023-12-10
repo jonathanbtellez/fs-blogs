f593d9c5ace284fd9a6334f7a394dea25297a357
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-10 15:20:56 -0500

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    const likes = blogs.map(blog => blog.likes)
+    return blogs.length === 0
+        ? 0
+        : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
