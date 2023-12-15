3cdd0917b80b7cefd0a06b37012b7ecc101c7323
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-15 18:01:26 -0500

    4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,13 +7,13 @@ const api = supertest(app)
 const Blog = require('../models/blog')
 const initialBlogs = [
   {
-    tile: 'Blog',
+    title: 'Blog',
     author: 'juna cat',
     url: 'url',
     likes: 58
   },
   {
-    tile: 'Blog 2',
+    title: 'Blog 2',
     author: 'juna cat',
     url: 'url',
     likes: 58
@@ -36,7 +36,30 @@ test('blogs are returned as json', async () => {
 
 test('identifier  id is difined', async () => {
   const blogs = await Blog.find({})
-  expect(blogs[0].id).toBeDefined() 
+  expect(blogs[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Blog 3',
+    author: 'juna cat',
+    url: 'url',
+    likes: 58
+  }
+
+  await api
+    .post('/api/v1/blogs')
+    .send(newBlog)
+
+  const blogsInDb = await Blog.find({})
+  console.log(blogsInDb)
+  expect(blogsInDb).toHaveLength(initialBlogs.length + 1)
+
+  const contents = blogsInDb.map(n => n.title)
+
+  expect(contents).toContain(
+    'Blog 3'
+  )
 })
 
 afterAll(() => {
