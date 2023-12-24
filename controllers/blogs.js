8160f81f13ef54890d6bce1aa67c47ad53b7a8e5
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-23 20:28:03 -0500

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
     if (!request.body.title) return response.status(400).end()
     if (!request.body.url) return response.status(400).end()
+    
+    const users = await User.find({})
 
-    request.body.likes = request.body.likes ? request.body.likes : 0
+    const blog = new Blog({
+        title: request.body.title,
+        url: request.body.url,
+        author: request.body.author,
+        likes: request.body.likes ? request.body.likes : 0,
+        user: users[0]._id
+    })
 
-    const blog = new Blog(request.body)
-    const result = blog.save()
+    const result = await blog.save()
+    users[0].blogs = users[0].blogs.concat(result._id) 
+    users[0].save()
     response.status(201).json(result)
 })
 
